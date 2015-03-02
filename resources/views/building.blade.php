623caa3b681e4a04fac085c8014745831a8d5193
Vongola <vongola12324@coder.tw>
2015-03-02 21:50:23 +0800

    Add Text

--- resources/views/building.blade.php
+++ resources/views/building.blade.php
@@ -5,9 +5,9 @@
     <div class="container">
         <div class="row text-center">
             {!! HTML::image('pic/building.jpg','Website is still building......'); !!}
-        </div>
-        <div class="progress text-center">
             <p>網頁建置中......</p>
+        </div>
+        <div class="progress">
             <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100" style="width: 30%">
                 <span class="sr-only">30% Complete</span>
             </div>
