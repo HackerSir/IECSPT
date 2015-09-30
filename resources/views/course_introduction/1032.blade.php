3612d4da5c03ee2bd82a03a7609b8802f50cd1bf
Chan, Danny <danny50610@yahoo.com.tw>
2015-10-01 01:50:33 +0800

    Independent out course intro.

---
+++ resources/views/course_introduction/1032.blade.php
@@ -0,0 +1,95 @@
+<div class="section green" id="courseIntroduction">
+    <div class="w-container">
+        <div class="w-row">
+            <div class="w-col w-col-4 align-right">
+                <h2>103下學期課程</h2>
+            </div>
+            <div class="w-col w-col-8">
+                <h2 class="aboutsectitle">程式設計競賽入門</h2>
+                <p>
+                    想要參加<strong>程式設計競賽</strong>，卻不知道怎麼準備嗎？<br/>
+                </p>
+                <h3 style="color: red">那你應該來聽這系列的課程</h3><br/>
+                <!--(有人會發現這行嗎？)講師CPE最高是4題、103 NCPC 決賽佳作-->
+                <p>
+                    地點：資電235<br/>
+                    時間：18:30 ~ 21:00
+                </p>
+                <p>
+                    有句話說：<br/>
+                </p>
+                <blockquote><p>資料結構 + 演算法 = 程式</p></blockquote>
+                <p>
+                    在競賽中，<strong>資料結構</strong>和<strong>演算法</strong>是必備的知識<br/>因此在入門課程裡會幫各位上點基礎的<strong>資料結構</strong>和<strong>演算法</strong>，<strong>資料結構</strong>比較會著重在使用，而不是實作，而<strong>演算法</strong>會著重概念上。
+                </p>
+                <ul>
+                    <li>資料結構</li>
+                    <ul>
+                        <li>vector</li>
+                        <li>linked list</li>
+                        <li>stack</li>
+                        <li>queue</li>
+                        <li>set</li>
+                        <li>heap</li>
+                        <li>graph</li>
+                        <li>map</li>
+                    </ul>
+                    <li>演算法</li>
+                    <ul>
+                        <li>時間複雜度</li>
+                        <li>DFS、BFS</li>
+                        <li>各個擊破</li>
+                        <li>動態規劃</li>
+                        <li>貪婪演算法</li>
+                        <li>圖論(最短路徑、最小生成樹)</li>
+                    </ul>
+                </ul><br/>
+                <h3 class="aboutsectitle">預備知識</h3>
+                <p>
+                    一些C語言和不屈不饒的精神
+                </p>
+                <h3 class="aboutsectitle">程式設計競賽</h3>
+                <ul>
+                    <li>全國大專電腦軟體設計競賽(NCPC)(<a href="http://ncpc.ntnu.edu.tw/ncpc/newslist.php">2014</a>)</li>
+                    <li>全國私立大專院校程式競賽(NCPU)(<a href="http://ncpu2014.pu.edu.tw/">2014</a>)</li>
+                    <li>ACM International Collegiate Programming Contest (ACM-ICPC)(<a href="http://icpc2014.pu.edu.tw/">2014TW</a>)</li>
+                    <li>ITSA(<a href="https://sites.google.com/site/itsancku/">ITSA月賽</a>)</li>
+                    <li><a href="http://ptc.moe.edu.tw/index.php">PTC</a></li>
+                    <li><a href="http://cpe.cse.nsysu.edu.tw/">CPE</a>(檢定)</li>
+                </ul><br/>
+                <h3 class="aboutsectitle">預定課表</h3>
+                <table class="CSSTableGenerator">
+                    <tr>
+                        <td>日期</td><td>課程名稱</td>
+                    </tr>
+                    <tr><td>2015 / 03 / 11</td><td>程式設計競賽入門(一)：陣列、鏈結串列</td></tr>
+                    <tr><td>2015 / 03 / 18</td><td>程式設計競賽入門(二)：堆疊、佇列</td></tr>
+                    <tr><td>2015 / 03 / 25</td><td>程式設計競賽相關網站介紹<br/>程式設計競賽入門(三)：時間複雜度、DFS、BFS</td></tr>
+                    <tr><td>2015 / 04 / 01</td><td>FILE I/O</td></tr>
+                    <tr><td>2015 / 04 / 08</td><td>Free Time (段考前一週)</td></tr>
+                    <tr><td>2015 / 04 / 15</td><td>休息(隔天期中考)</td></tr>
+                    <tr><td>2015 / 04 / 22</td><td>期中考周(04/16~04/22)</td></tr>
+                    <tr><td>2015 / 04 / 29</td><td>程式設計競賽入門(四)：樹狀結構、Map</td></tr>
+                    <tr><td>2015 / 05 / 06</td><td>程式設計競賽入門(五)：graph、圖論(最短路徑、最小生成樹)</td></tr>
+                    <tr><td>2015 / 05 / 13</td><td>程式設計競賽入門(六)：各個擊破、動態規劃、貪婪演算法</td></tr>
+                    <tr><td>2015 / 05 / 20</td><td>尚未安排</td></tr>
+                    <tr><td>2015 / 05 / 27</td><td>尚未安排</td></tr>
+                    <tr><td>2015 / 06 / 03</td><td>尚未安排</td></tr>
+                    <tr><td>2015 / 06 / 10</td><td>Free Time (段考前一週)</td></tr>
+                    <tr><td>2015 / 06 / 17</td><td>休息(隔天期末考)</td></tr>
+                    <tr><td>2015 / 06 / 24</td><td>期末考周(06/18~06/26)</td></tr>
+                </table><br/>
+                <P style="font-size: 10pt">
+                    UpdateLog:<br />
+                    2015/03/09: 第一次建立
+                </P>
+                <br />
+                <h3 class="aboutsectitle">開課緣由</h3>
+                <p>
+                    上學期期末有開會，決議教程式設計競賽
+                </p>
+                <br>
+            </div>
+        </div>
+    </div>
+</div>
