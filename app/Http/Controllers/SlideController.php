9dcffe3edfd01125bd63d79cd08a13d93beb4b79
Chan, Danny <danny50610@yahoo.com.tw>
2015-10-02 14:45:46 +0800

    Format code

--- app/Http/Controllers/SlideController.php
+++ app/Http/Controllers/SlideController.php
@@ -6,29 +6,29 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
 
-class SlideController extends Controller {
+class SlideController extends Controller
+{
 
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-
-        $year = (int)date("Y",time());
-        $month = (int)date("M",time()) ;
-        if($month < 8)
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        $year = (int)date("Y", time());
+        $month = (int)date("M", time());
+        if ($month < 8)
             $year -= 1;
         $year -= 1911;
         $classList = File::allFiles("partials");
         $classList = array_reverse($classList);
         $yearList = [];
-        foreach($classList as $key => $value){
-            $yearList[$key] = str_replace('partials'.DIRECTORY_SEPARATOR,'',str_replace('.html','',$value));
+        foreach ($classList as $key => $value) {
+            $yearList[$key] = str_replace('partials' . DIRECTORY_SEPARATOR, '', str_replace('.html', '', $value));
         }
 
-		return view('slides') -> with("classList", $classList) -> with("yearList", $yearList);
-	}
+        return view('slides')->with("classList", $classList)->with("yearList", $yearList);
+    }
 
 }
